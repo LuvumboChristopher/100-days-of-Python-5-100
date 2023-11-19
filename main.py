0dfb5a9ca9fe7f386260f893f6aa222eb00d4be1
Christopher Luvumbo <christopherluvumbo.dev@gmail.com>
2023-11-19 18:45:22 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+print("Marvel Movie Character Creator")
+
+answerOne = input("Do you like 'hanging around'? ")
+if answerOne == "yes":
+  print("Then you're  Spider-man")
+else:
+  answerTwo = input("Do you like 'sports'? ")
+  if answerTwo == "yes":
+    print("Then you're Batman")
+  else:
+    answerThree = input("Do you like 'action'? ")
+    if answerThree == "yes":
+      print("Then you're Ant man")
+    else:
+      answerFour = input("Do you have a 'gravelly' voice? ")
+      if answerFour == "yes":
+        print("Aww, then you're Korg")
+      else:
+        answerFive = input("Do you like 'superheroes'? ")
+        if answerFive == "yes":
+          print("Then you're Iron Man")
+        else:
+          answerSix = input("Do you like 'comedy'? ")
+          if answerSix == "yes":
+            print("Then you're Black Panther")
+          else:
+            answerSeven = input("Do you like 'fighting'? ")
+            if answerSeven == "no":
+              print("Then you're Hulk")
+            else:
+              answerEight = input("Do you like 'stealth'? ")
+              if answerEight == "yes":
+                print("Then you're Thor")
+              else:
+                answerNine = input("Do you like 'magic'? ")
+                if answerNine == "no":
+                  print("Then you're Doctor Strange")
+                else:
+                  answerTen = input("Do you like 'flying'? ")
+                  if answerTen == "no ":
+                    print("Then you're Captain America")
